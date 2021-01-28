0995f769fb257095c736ebea5c73955888f238bc
Lee <lhaney8891@gmail.com>
2021-01-27 21:17:08 -0500

    Fixed bug in script.js

--- js/script.js
+++ js/script.js
@@ -58,7 +58,7 @@ function create_gallery_markup(data) {
             <img class="card-img" src="${employee.picture.medium}" alt="profile picture">
         </div>
         <div class="card-info-container">
-            <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
+            <h3 id="name" class="card-name cap">${employee.name.first}</h3>
             <p class="card-text">${employee.email}</p>
             <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
         </div>
@@ -86,7 +86,7 @@ function locate_employee_object(name, num) {
     let name_array = name.split(" ");
     if (num === 0) {
         const obj = all_employees.filter(function(employee) {
-            if (employee.name.first === name_array[0] && employee.name.last === name_array[1]) {
+            if (employee.name.first === name_array[0]) {
                 return employee;
             } 
         });
@@ -95,7 +95,7 @@ function locate_employee_object(name, num) {
     // This is for when the user clicks the "NEXT" button on the modal window
     else if (num === 1) {
         for (let i = 0; i < all_employees.length; i++) {
-            if (all_employees[i].name.first === name_array[0] && all_employees[i].name.last === name_array[1]) {
+            if (all_employees[i].name.first === name_array[0]) {
                 let obj = all_employees[i+1];
                 if ( (i+1) > 11 ) {
                     obj = all_employees[0];
@@ -107,7 +107,7 @@ function locate_employee_object(name, num) {
     // This is for when the user clicks the "PREV" button on the modal window
     else if (num === 2) {
         for (let i = 0; i < all_employees.length; i++) {
-            if (all_employees[i].name.first === name_array[0] && all_employees[i].name.last === name_array[1]) {
+            if (all_employees[i].name.first === name_array[0]) {
                 let obj = all_employees[i-1];
                 if ( (i-1) < 0) {
                     obj = all_employees[11];
@@ -132,15 +132,9 @@ function create_search_markup() {
     input_one.setAttribute('class', 'search-input');
     input_one.setAttribute('placeholder', 'Search...');
     
-
-    let input_two = document.createElement('INPUT');
-    input_two.setAttribute('type', 'submit');
-    input_two.setAttribute('value', 'SEARCH',);
-    input_two.setAttribute('id', 'serach-submit');
-    input_two.setAttribute('class', 'search-submit');
-
     search_container.appendChild(form);
-    form.append(input_one, input_two);
+    // form.append(input_one, input_two);
+    form.append(input_one);
 }
 
 /*
@@ -207,12 +201,12 @@ function add_information_to_modal_window(human) {
 
     let html = `
     <img class="modal-img" src="${person.picture.medium}" alt="profile picture">
-    <h3 id="name" class="modal-name cap">${person.name.first} ${person.name.last}</h3>
+    <h3 id="name" class="modal-name cap">${person.name.first}</h3>
     <p class="modal-text">${person.email}</p>
     <p class="modal-text cap">${person.location.city}</p>
     <hr>
     <p class="modal-text">${person.cell}</p>
-    <p class="modal-text cap">${person.location.street} ${person.location.city}, ${person.location.state} ${person.location.postcode}</p>
+    <p class="modal-text cap">${person.location.city}, ${person.location.state} ${person.location.postcode}</p>
     <p class="modal-text">Birthday: ${birthdate}</p>
     `;
     document.querySelector('.modal-info-container').innerHTML = html;
@@ -260,6 +254,8 @@ If a user
 Different employee cards will disappear or reappear.
 */
 function hide_or_display_employees(typed_name) {
+    console.log("Typed name: ", typed_name);
+
     const all_card_divs = document.getElementById('gallery');
     const people = all_card_divs.children;
 
@@ -271,7 +267,7 @@ function hide_or_display_employees(typed_name) {
     for (let i = 0; i < people.length; i++) {
         const item = people[i];
         const name = item.querySelector('#name');
-        if (name.textContent.includes(typed_name.toLowerCase())) {
+        if (name.textContent.toLowerCase().includes(typed_name.toLowerCase()) ) {
             item.style.display = "";
             found_length += 1;
         } else {
@@ -279,9 +275,12 @@ function hide_or_display_employees(typed_name) {
             hidden_length += 1;
         }
     } 
+    console.log("hidden_length: ", hidden_length)
+    console.log("found_length: ", found_length)
 
     if (hidden_length === 12 && found_length === 0) {
         if (!no_employees_found_message_exist) {
+            // Make error message appear
             let html = `No employees found!`;
             error_div.innerHTML = html;
             gallery.insertAdjacentElement("afterend", error_div);
@@ -308,17 +307,11 @@ getRandomUsers(random_url)
   .catch(err => console.log(err))
 
 
-// Search Button (top-right corner of page)
-document.getElementById('serach-submit').addEventListener('click', function(e) {
-    e.preventDefault();
-    let user_input = document.getElementById('search-input').value;
-    hide_or_display_employees(user_input);
-});
-
 // Text Box for the Search Button (top-right corner of page)
 document.getElementById('search-input').addEventListener('keyup', function(event) {
     let user_input = document.getElementById('search-input').value;
     hide_or_display_employees(user_input);
+
 });
 
 
